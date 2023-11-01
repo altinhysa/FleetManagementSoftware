6d86ee8f6fc7b6e1b0da21b4c98cf3f024a65706
altin hysa <altinhysa84@gmail.com>
2023-11-01 13:35:19 +0100

    first commit

---
+++ src/main/java/com/spines/fleetmanagementsoftware/FleetManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.spines.fleetmanagementsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spines/fleetmanagementsoftware/FleetManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spines.fleetmanagementsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
